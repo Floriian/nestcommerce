2746726da30c7c7e0ddd391a77dfb76b1e28fb37
Floriian <lovasifloriian@outlook.hu>
2024-04-27 23:00:39 +0200

    validation error mapping

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { validationPipeConfig } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: { origin: ['http://localhost:5173'] },
   });
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe(validationPipeConfig));
 
   await app.listen(3000);
 }
