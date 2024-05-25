f9a5054e5d7601b197606490fafe1dca90fea2a6
Floriian <lovasifloriian@outlook.hu>
2024-05-25 09:48:03 +0200

    prepare for fileuplaod

--- apps/admin/src/features/category/category.schema.ts
+++ apps/admin/src/features/category/category.schema.ts
@@ -4,6 +4,7 @@ export const categorySchema = z.object({
   _id: z.string().optional(),
   name: z.string(),
   active: z.boolean().default(true),
+  image: z.instanceof(File).optional(),
 });
 
 export type Category = z.infer<typeof categorySchema>;
